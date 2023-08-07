a9d70a74dd4bbd07e6e9af9c8ed16cc5d1762e7b
dae-ne <rafalcz29@gmail.com>
2023-08-08 00:29:13 +0200

    Add unit tests

--- action/csproj.js
+++ action/csproj.js
@@ -1,18 +1,12 @@
 import { readFileSync } from 'fs';
-import { DOMParser } from 'xmldom';
+import { DOMParser } from '@xmldom/xmldom';
 import xpath from 'xpath';
 
 export const readFile = (path) => {
-  return readFileSync(path, 'utf8');
+  return readFileSync(path, 'utf8').trim();
 };
 
 export const getVersionFromFile = (file) => {
   const doc = new DOMParser().parseFromString(file, 'text/xml');
-  const nodes = xpath.select('string(/Project/PropertyGroup/Version)', doc);
-
-  if (nodes.length < 1) {
-    throw new Error('Version not found');
-  }
-
-  return nodes[0];
+  return xpath.select('string(/Project/PropertyGroup/Version)', doc).trim();
 };
---
+++ tests/csproj.test.js
@@ -0,0 +1,33 @@
+import { readdirSync } from 'fs';
+import { getVersionFromFile, readFile } from '../action/csproj';
+
+describe('readFile function', () => {
+  it('should read existing file', () => {
+    const file = readFile('tests/testFiles/ValidFile1_1.0.0.csproj');
+    expect(typeof(file)).toBe('string');
+    expect(file.length).not.toEqual(0);
+  });
+
+  it('should throw an exception when file does not exist', () => {
+    try {
+      readFile('tests/testFiles/NonExistingFile.csproj');
+    } catch (error) {
+      expect(error.code).toEqual('ENOENT');
+    }
+  });
+});
+
+describe('getVersionFromFile function', () => {
+  it('should return version from files', () => {
+    const files = readdirSync('tests/testFiles')
+      .filter(file => file.startsWith('ValidFile') &&
+                      file.endsWith('.csproj' &&
+                      file.includes('1.0.0')));
+
+    files.forEach(file => {
+      const fileContent = readFile(`tests/testFiles/${file}`);
+      const version = getVersionFromFile(fileContent);
+      expect(version).toEqual('1.0.0');
+    });
+  });
+});
