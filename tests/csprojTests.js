05b99a759542a1633bd3bd3232e602df0d5e6d94
dae-ne <rafalcz29@gmail.com>
2023-08-08 02:01:51 +0200

    Update path to test files

--- tests/csprojTests.js
+++ tests/csprojTests.js
@@ -3,14 +3,14 @@ import { getVersionFromFile, readFile } from '../src/csproj';
 
 describe('readFile function', () => {
   it('should read existing file', () => {
-    const file = readFile('tests/testFiles/ValidFile1_1.0.0.csproj');
+    const file = readFile('tests/TestFiles/ValidFile1_1.0.0.csproj');
     expect(typeof(file)).toBe('string');
     expect(file.length).not.toEqual(0);
   });
 
   it('should throw an exception when file does not exist', () => {
     try {
-      readFile('tests/testFiles/NonExistingFile.csproj');
+      readFile('tests/TestFiles/NonExistingFile.csproj');
     } catch (error) {
       expect(error.code).toEqual('ENOENT');
     }
@@ -19,13 +19,13 @@ describe('readFile function', () => {
 
 describe('getVersionFromFile function', () => {
   it('should return version from files', () => {
-    const files = readdirSync('tests/testFiles')
+    const files = readdirSync('tests/TestFiles')
       .filter(file => file.startsWith('ValidFile') &&
                       file.endsWith('.csproj' &&
                       file.includes('1.0.0')));
 
     files.forEach(file => {
-      const fileContent = readFile(`tests/testFiles/${file}`);
+      const fileContent = readFile(`tests/TestFiles/${file}`);
       const version = getVersionFromFile(fileContent);
       expect(version).toEqual('1.0.0');
     });
